93c5c399d6a5b2bd05ac44d2798fed7f7629b77f
flora <tingyu1112@gmail.com>
2023-05-15 01:00:20 +0800

    first commit

---
+++ all.js
@@ -0,0 +1,16 @@
+
+//題目一：字串長度計算
+function checkStringLength(string){
+    return string.length
+    }
+      
+    console.log(checkStringLength("string"));
+ //題目二：英文強制小寫
+ function lowerCase(string){
+     return (string).toLowerCase()
+ }
+ 
+console.log(lowerCase("FOKFF@gmail.com"))
+console.log(lowerCase("gonSakpm@gmail.com"))
+
+//123
